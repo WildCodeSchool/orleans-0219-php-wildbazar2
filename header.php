f8c74d2a2d04b9671ebcf6174dbd1d5466734828
jiceR <jc-roger45000@protonmail.com>
2019-03-13 15:52:29 +0100

    add variables php in header

--- header.php
+++ header.php
@@ -26,85 +26,16 @@
 		</div>
 		<button type="button" class="btn btn-warning pull-right">Cart <i class="fas fa-cart-arrow-down"></i></button>
 	</nav>
-
-	<div class="bd-example">
-		<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-			<ol class="carousel-indicators">
-				<li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-				<li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-				<li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-				<li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
-				<li data-target="#carouselExampleCaptions" data-slide-to="4"></li>
-				<li data-target="#carouselExampleCaptions" data-slide-to="5"></li>
-			</ol>
-			<div class="carousel-inner">
-				<div class="carousel-item active">
-					<a href="#home">
-					<img class="carousel_image homeImg" src="/images/banner_image_original_1509469545.jpg" class="d-block w-100" alt="commercial image">
-					</a>
-					<div class="carousel-caption d-none d-md-block">
-					<h1 class="carousel_title3">Wild Bazar</h1>
-					<p class="carousel_text3">You can find everything and especially anything</p>
-				</div>
-			</div>
-			<div class="carousel-item">
-				<a href="/categories/aquarium.php">
-				<img class="carousel_image  homeImg" src="/images/fish.jpg" class="d-block w-100" alt="fishs">
-				</a>
-				<div class="carousel-caption d-none d-md-block">
-					<h2 class="carousel_title3">Pet shop</h2>
-					<p class="carousel_text3">All for your fishs</p>
-				</div>
-			</div>
-
-			<div class="carousel-item">
-				<a href="fashion.php">	
-				<img class="carousel_image homeImg" src="/images/fashion3.jpg" alt="wears">
-				</a>
-				<div class="carousel-caption d-none d-md-block">
-					<h2 class="carousel_title3">Fashion</h2>
-					<p class="carousel_text3">All fashion is here !</p>
-				</div>
-			</div>
-			<div class="carousel-item">
-				<a href="watches-items.php">
-				<img class="carousel_image homeImg" src="/images/montres.jpg" alt="watch">
-				</a>
-				<div class="carousel-caption d-none d-md-block">
-					<h2 class="carousel_title3">Watches</h2>
-					<p class="carousel_text3">Efficient, stylish, and on top of that, they give you the good time !</p>
-				</div>
-			</div>
-			<div class="carousel-item">
-			<a href="categories-tech.php">
-			<img class="carousel_image homeImg" src="/images/tech.jpg" alt="computer">
-			</a>
-				<div class="carousel-caption d-none d-md-block">
-					<h2 class="carousel_title3">High-tech</h2>
-					<p class="carousel_text3">The best of high-tech !</p>
-				</div>
-			</div>
-			<div class="carousel-item">
-				<a href="electromenager.php">
-				<img class="carousel_image homeImg" src="/images/cuisine.jpg" alt="kitchen">
-				</a>
-				<div class="carousel-caption d-none d-md-block">
-					<h2 class="carousel_title3">Home appliance</h2>
-					<p class="carousel_text3">Fashion equipment for cooking and housekeeping</p>
-				</div>
-			</div>
-		</div>
-			<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-			<span class="sr-only">Previous</span>
-			</a>
-			<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-			<span class="carousel-control-next-icon" aria-hidden="true"></span>
-			<span class="sr-only">Next</span>
-			</a>
-		</div>
-	</div>
-	<div>
+		<?php
+		
+			if('/index.php'== $_SERVER['PHP_SELF']){
+				include 'carousel.php';
+			}
+			else{
+				include 'banner.php';
+			}
+		?>
+	
         <div class="<?php echo $classValueBanner; ?>">
             <img src="<?php echo $imgBannerSrc; ?>" class="img-fluid" alt="category image"/>
             <h1><?php echo $titleBanner; ?></h1>
