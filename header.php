57705c12510509bf6c2142a95cfa140ee7f88217
Julien Rousseau <julien.rousseau@hotmail.fr>
2019-03-13 15:24:34 +0100

    modif alt img banner

--- header.php
+++ header.php
@@ -68,11 +68,11 @@
 
         <div>
             <div class="<?php echo $classValueBanner; ?>">
-                <img src="<?php echo $imgBannerSrc; ?>" class="img-fluid" alt="home appliance"/>
+                <img src="<?php echo $imgBannerSrc; ?>" class="img-fluid" alt="category image"/>
                 <h1><?php echo $titleBanner; ?></h1>
                 <p><?php echo $textBanner; ?></p>
             </div>
         </div>
 
-  
+
     </header>
